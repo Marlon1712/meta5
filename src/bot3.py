5a348d54064bc723989eff5c687f5b36a369615a
Marlon1712 <marlonubl84@gmail.com>
2021-12-24 07:33:36 -0300

    atualização

--- src/bot3.py
+++ src/bot3.py
@@ -10,12 +10,16 @@ def hello(update: Update, context: CallbackContext) -> None:
 def meta(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'{update.effective_user.first_name} este é um teste de resposta')
 
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'{update.effective_user.first_name}, vc esta pronto para começar')
+
 token = os.getenv('MY_TOKEN')
 
 updater = Updater(token)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('meta', meta))
+updater.dispatcher.add_handler(CommandHandler('start', start))
 
 updater.start_polling()
 updater.idle()
